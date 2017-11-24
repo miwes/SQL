c5598b190f49d9f43b18504395f5db0fd692f7e2
miwes <weis@outlook.cz>
2017-11-24 11:06:05 +0100

    Update Show_All_request.sql

--- DMV/Show_All_request.sql
+++ DMV/Show_All_request.sql
@@ -1,14 +1,14 @@
 SELECT
-	DER.session_id				AS [Blocked session ID]
-	,DER.status					AS [Blocked status]
-	,DER.start_time				AS [Blocked session start time]
-	,DER.wait_time / 1000		AS [Blocked time (s)]
-	,DER.wait_type				AS [Blocked wait type]
+	DER.session_id				AS [Session ID]
+	,DER.status					AS [Status]
+	,DER.start_time				AS [Session start time]
+	,DER.wait_time / 1000		AS [Wait time (s)]
+	,DER.wait_type				AS [Wait type]
 	,DES.host_name
 	,(SELECT text FROM sys.fn_get_sql(DER.sql_handle)) AS SQLText
 	,DES.cpu_time
+	,DER.last_wait_type
 FROM sys.dm_exec_requests AS DER
 LEFT JOIN sys.dm_exec_sessions AS DES		ON DER.session_id = DES.session_id
-WHERE DER.status = 'running'
+--WHERE DER.status = 'running'
 ORDER BY cpu_time DESC
---WHERE DES.host_name LIKE 'WINPCAPA%'
